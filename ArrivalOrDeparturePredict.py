0dc8f357458f16867d424ec7ab3c02f5b169cdcc
alex.simion <alexandru.simion@jlg.ro>
2021-10-26 21:53:59 +0300

    Initial commit

---
+++ ArrivalOrDeparturePredict.py
@@ -0,0 +1,54 @@
+import pandas as pd
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+df = pd.read_csv("aviation-flight-information.csv")
+
+types_dict = {'Arrive or Depart': str, 'Schedule': str, 'Airline': str, 'Flight Number': str, 'Gate': str,
+              'Terminal': float}
+for col, col_type in types_dict.items():
+    df[col] = df[col].astype(col_type)
+
+le = preprocessing.LabelEncoder()
+
+arriveOrDepart_enc = le.fit_transform(df["Arrive or Depart"].values)
+# print(arriveOrDepart_enc)
+
+airline_enc = le.fit_transform(df["Airline"].values)
+# print(airline_enc)
+
+gate_enc = le.fit_transform(df["Gate"].values)
+# print(gate_enc)
+
+terminal_enc = le.fit_transform(df["Terminal"].values)
+# print(terminal_enc)
+
+y = arriveOrDepart_enc
+
+X = list(zip(gate_enc, terminal_enc, airline_enc))
+
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size=0.20,
+                                                    random_state=22)
+
+# print("X_train", X_train)
+# print("X_test", X_test)
+# print("y_train", y_train)
+# print("y_test", y_test)
+
+rfc = RandomForestClassifier()
+
+rfc.fit(X_train, y_train)
+
+print(rfc.score(X_train, y_train))
+print(rfc.score(X_test, y_test))
+df["Arrive or Depart Prediction Numerical"] = rfc.predict(X)
+
+df.assign(ArriveOrDepartPrediction=pd.cut(df["Arrive or Depart Prediction Numerical"], bins=[-1, 0, 1],
+                                          labels=['Arrival', 'Departure'])).to_csv("prediction.csv",
+                                                                                   encoding='utf-8', index=False)
+
+rfc_imp = pd.DataFrame(rfc.feature_importances_, columns=['Importance'])
+rfc_imp["Importance"] = rfc_imp["Importance"] * 100
+print(rfc_imp["Importance"])
